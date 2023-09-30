01c1d0f07a64b5954f98f79f217edf0a0dcf2495
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-09-30 17:59:09 +0700

    SeminarOne Finish

---
+++ SeminarOne/main.java
@@ -0,0 +1,23 @@
+package SeminarOne;
+
+public class main {
+    public static void main(String[] args) {
+        /*
+         * Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное
+         * количество подряд идущих 1.
+         */
+        int[] array = new int[] { 1, 1, 0, 1, 1, 1 };
+        int count = 0;
+        int maxCount = 0;
+        for (int i : array) {
+            if (i == 1) {
+                count++;
+                if (maxCount <= count)
+                    maxCount = count;
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(maxCount);
+    }
+}
