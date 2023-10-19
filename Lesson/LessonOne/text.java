f38e5d492af99f95aaa8548c1d7f9e5ebf070406
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-10-19 19:35:09 +0700

    Lesson One Up

---
+++ Lesson/LessonOne/text.java
@@ -0,0 +1,10 @@
+package Lesson.LessonOne;
+
+public class text {
+    public static void main(String[] args) {
+        int d = 123;
+        System.out.println(d);
+        d = d-- - --d;
+        System.out.println(d);
+    }
+}
